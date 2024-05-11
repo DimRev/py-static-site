b8f967b4ef0375ef556d4086f3e9b2fd3b7f60b3
DimRev <dimrev444@gmail.com>
2024-05-11 14:07:14 +0300

    implement text_to_textnode func

--- src/main.py
+++ src/main.py
@@ -4,7 +4,40 @@ from parentnode import ParentNode
 import re
 
 def main():
-  pass
+  text_nodes = text_to_textnodes("This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)")
+
+  print(text_nodes)
+
+def text_to_textnodes(text):
+  default_node = TextNode(text, TextType.TEXT)
+  
+  split_on_bold = split_delimiter(default_node, "**", TextType.BOLD)
+
+  bold_italic = []
+  for block in split_on_bold:
+    split_on_italic = split_delimiter(block, "*", TextType.ITALIC)
+    bold_italic.extend(split_on_italic)
+
+  bold_italic_code = []
+  for block in bold_italic:
+    split_on_code = split_delimiter(block, "`", TextType.CODE)
+    bold_italic_code.extend(split_on_code)
+
+  bold_italic_code_image = []
+  for block in bold_italic_code:
+    split_on_image = split_nodes_image(block)
+    bold_italic_code_image.extend(split_on_image)
+
+  bold_italic_code_image_link = []
+  for block in bold_italic_code_image:
+    if block.text_type == TextType.IMAGE:
+      bold_italic_code_image_link.append(block)  
+      continue
+    split_on_link = split_nodes_links(block)
+    bold_italic_code_image_link.extend(split_on_link)
+
+  
+  return bold_italic_code_image_link
 
 def text_node_to_html_node(text_node: TextNode):
   text_type = text_node.text_type
