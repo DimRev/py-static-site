7cd147e9355f1d7438ce1fd540460ee26991cedf
DimRev <dimrev444@gmail.com>
2024-05-11 11:47:56 +0300

    Add split_delimiter function

--- src/main.py
+++ src/main.py
@@ -1,29 +1,48 @@
-from textnode import TextNode
+from textnode import TextNode,TextType
 from leafnode import LeafNode
+from parentnode import ParentNode
 
 def main():
-  url_link = TextNode("This is a text node", "bold", "https://boot.dev")
-  img = TextNode("Test", "image", "http://example.com")
-  img_node = text_node_to_html_node(img)
-  print(img_node.to_html())
+  code_node = TextNode("`This` is a `code` block `text`",TextType.TEXT)
+  text_nodes = split_delimiter(code_node, "`", TextType.CODE)
+  print(text_nodes)
 
 def text_node_to_html_node(text_node: TextNode):
   text_type = text_node.text_type
   text = text_node.text
   url = text_node.url
-  if text_type == 'text':
+  if text_type == TextType.TEXT:
     return LeafNode(None, text)
-  elif text_type == 'bold':
+  elif text_type == TextType.BOLD:
     return LeafNode("b", text)
-  elif text_type == 'italic':
+  elif text_type == TextType.ITALIC:
     return LeafNode("i", text)
-  elif text_type == 'code':
+  elif text_type == TextType.CODE:
     return LeafNode("code", text)
-  elif text_type == 'link':
+  elif text_type == TextType.LINK:
     return LeafNode("a", text, {"href": url})
-  elif text_type == 'image':
+  elif text_type == TextType.IMAGE:
     return LeafNode("img", None, {"src":url, "alt":text})
   else:
     raise Exception("text type not supported")
+
+def split_delimiter(old_node: TextNode, delimiter, text_type: TextType):
+    text = old_node.text
+    
+    blocks = text.split(delimiter)
+    if len(blocks) % 2 == 0:
+      raise Exception("missing closing delimiter")
+    text_nodes = []
+    for i, block in enumerate(blocks):
+        if i % 2 == 0:  
+            if block == "":
+              continue
+            text_node = TextNode(block, old_node.text_type)
+            text_nodes.append(text_node)  
+        else:
+            text_node = TextNode(block, text_type)    
+            text_nodes.append(text_node)
+            
+    return text_nodes
 
 main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,14 @@
+from enum import Enum
+class TextType(Enum):
+  TEXT = 'text'
+  BOLD = 'bold'
+  ITALIC = 'italic'
+  CODE = 'code'
+  LINK = 'link'
+  IMAGE = 'image'
+
 class TextNode():
-  def __init__(self, text, text_type, url= None):
+  def __init__(self, text, text_type:TextType, url= None):
     self.text = text
     self.text_type = text_type
     self.url = url
